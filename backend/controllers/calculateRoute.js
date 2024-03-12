0302f5d43d63d21dc0d880b9d3521c5ad1d82ba8
Gabriel Sousa <jgss.gabriel.sousa@gmail.com>
2024-03-11 22:11:42 -0300

    algorithm improvement

--- backend/controllers/calculateRoute.js
+++ backend/controllers/calculateRoute.js
@@ -1,6 +1,15 @@
 const math = require("mathjs");
 const db = require("../database");
 
+let facilitaOBJ = { 
+    id: 0,
+    name: "Facilita Jurídico",
+    email: "facilita@facilita.juridico",
+    phone: "559999999",
+    x: 0, 
+    y: 0
+}
+
 const calculateRoute = async (req, res) => {
     try {
         const clients = await db.query("SELECT * FROM clientes");
@@ -14,35 +23,76 @@ const calculateRoute = async (req, res) => {
             y: client.coord_y,
         }));
 
-        const calcDistance = (pointA, pointB) => math.distance([pointA.x, pointB.y], [pointA.x, pointB.y]);
+        coordinates.unshift(facilitaOBJ);
 
-        const findNearestNeighbor = (point, availablePoints) => {
-            let minimumDistance = Number.MAX_SAFE_INTEGER;
-            let nearestNeighbor = null;
+        let graph = {};
 
-            for (const availablePoint of availablePoints) {
-                const distance = calcDistance(point, availablePoint);
+        for(const client of coordinates) {
+            graph[client.id] = {};
 
-                if (distance < minimumDistance) {
-                    minimumDistance = distance;
-                    nearestNeighbor = availablePoint;
+            for(const c of coordinates) {
+                if(c.id == client.id || client.id == c.id) continue;
+
+                graph[client.id][c.id] = math.distance([client.x, client.y], [c.x, c.y]);
+            }
+        }
+
+        function calculateDijkstra(graph, origem) {
+            const distancias = {};
+            const visitados = new Set();
+        
+            // Inicialização das distâncias com infinito, exceto a origem que é zero
+            for (let v in graph) {
+                distancias[v] = Infinity;
+            }
+            distancias[origem] = 0;
+        
+            while (visitados.size !== Object.keys(distancias).length) {
+                // Encontra o vértice não visitado com menor distância atual
+                let verticeAtual = null;
+                let menorDistancia = Infinity;
+                for (let v in graph) {
+                    if (!visitados.has(v) && distancias[v] < menorDistancia) {
+                        verticeAtual = v;
+                        menorDistancia = distancias[v];
+                    }
+                }
+        
+                // Marca o vértice atual como visitado
+                visitados.add(verticeAtual);
+        
+                // Atualiza as distâncias dos vértices vizinhos
+                for (let vizinho in graph[verticeAtual]) {
+                    const peso = graph[verticeAtual][vizinho];
+                    if (distancias[verticeAtual] + peso < distancias[vizinho]) {
+                        distancias[vizinho] = distancias[verticeAtual] + peso;
+                    }
                 }
             }
-
-            return nearestNeighbor;
+        
+            // Retorna as distâncias mais curtas a partir da origem
+            return distancias;
         }
 
-        const route = [{ id: 0, name: "Facilita Jurídico", email: "facilita.juridico@facilita.juridico", phone: "99 99999999", x: 0, y: 0 }];
-        let currentPoint = { x: 0, y: 0 }
+        const result = calculateDijkstra(graph, "0");
+        
+        // Convertendo o objeto em uma matriz de pares chave-valor e ordenando a matriz com base nos valores
+        const keyValuePairs = Object.entries(result);
+        const sortedArray = keyValuePairs.sort((a, b) => a[1] - b[1]);
+    
+        // Extraindo o valor das chaves
+        const routeIDS = sortedArray.map(pair => pair[0]);
 
-        while (coordinates.length > 0) {
-            const nearestNeighbor = findNearestNeighbor(currentPoint, coordinates)
-            route.push(nearestNeighbor);
-            currentPoint = nearestNeighbor;
-            coordinates.splice(coordinates.findIndex((point) => point.id === nearestNeighbor.id), 1);
-        }
-
-        route.push({ id: 0, name: "Facilita Jurídico", email: "facilita.juridico@facilita.juridico", phone: "99 99999999", x: 0, y: 0 });
+        const sortedData = [];
+        
+        routeIDS.forEach(id => {
+            const obj = coordinates.find(item => item.id.toString() === id);
+            if (obj) {
+            sortedData.push(obj);
+            }
+        });
+        const route = sortedData;
+        route.push(facilitaOBJ);
 
         return res.status(200).json(route);
 
